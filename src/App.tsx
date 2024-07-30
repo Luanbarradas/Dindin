1bbaf74c7e7029172b7903cbfaece4f3c7ae1660
marcelo-dds <marcelodds_web@hotmail.com>
2024-07-30 19:29:21 -0300

    feat(registration.modules.css.registration.tsx):created registration and created css modules

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
+import { Registration } from "./components/resgistration";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <Main />
+      <Registration />
     </>
   );
 };
