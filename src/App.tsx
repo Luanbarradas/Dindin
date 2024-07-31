9645dae47c4c295804fbc67c1da3858920eb9feb
Luanbarradas <barradas.luk@gmail.com>
2024-07-30 21:29:15 -0300

    feat: SignUp in Main

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,15 @@ import "./App.css";
 
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
-import { SignUp } from "./components/SignUp";
-import { SignIn } from "./components/SignIn";
+// import { SignUp } from "./components/SignUp";
+// import { SignIn } from "./components/SignIn";
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <Main />
-      <SignUp />
+
       {/* <SignIn /> */}
     </>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { SignUp } from "./SignUp";
+
 import styles from "./Main.module.css";
 
 export const Main: React.FC = () => {
-  return <main className={styles.bg_main}></main>;
+  return (
+    <main className={styles.bg_main}>
+      <SignUp />
+    </main>
+  );
 };
